120506ef7eb660078607dda58f6d725ac119fcbf
Tejinder Pal Singh <dev.singhtejinder@gmail.com>
2019-09-05 16:32:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ResourceCursorAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Button ad = findViewById(R.id.add);
+//        Intent s = new Intent(this, naviActivit.class);
+//
+//        startActivity(s);
+
+    }
+}
